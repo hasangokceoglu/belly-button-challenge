1fb181d5f9f01c4ef50bd14b6d2b783870c23380
hasangokceoglu <118228120+hasangokceoglu@users.noreply.github.com>
2023-05-12 00:16:30 +0100

    updated bubble chart and demographics

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,44 +1,105 @@
-// Use the D3 library to read in samples.json from the URL
-function buildMetadata(sample) {
-  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json")
-    let metadata=data.metadata;
+const sample_url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-    // Filter the data for the object with the desired sample number
-
-    let idArray= metadata.filter(dataObj.id == sample);
-
-    let result = idArray[0]
-
-    // Use d3 to select the panel with id of '#sample-metadata'
-    let PANEL = d3.select("#sample-metadata");
-
-    // Use '.html("" to clear any existing metadata)
-    PANEL.html("");
-
-    // hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    for (key in result) {
-      PANEL.append("h6").text(`${key.toUpperCase()}: ${result[key]}`);
-    };
-  }
-
-  function buildCharts(sample) {
-
-    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json") 
-      let metadata = data.metadata;
-      // Filter the data for the object with the desired sample number
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+function bar_chart(value) {
+  d3.json(sample_url).then(function(data) {
+      let results = data.samples.filter(x => x.id == value);
+  
+      let trace_bar = [{
+          x: results[0].sample_values.slice(0, 10).reverse(),
+          y: results[0].otu_ids.slice(0, 10).map(z => "OTU " + z.toString()).reverse(),
+          type: "bar",
+          orientation: "h",
+          text: results[0].otu_labels.slice(0, 10).reverse(),
+      }];
+  
       
-      let idArray = metadata.filter(dataObj => dataObj.id == sample);
-      let result = idArray[0];
-
-      let otu_ids = result.otu_ids;
-      let otu_labels = result.otu_labels;
-      let sample_values = result.cample_values;
+      Plotly.newPlot("bar", trace_bar);
   }
+)};
+
+// Create a bubble chart that displays each sample    
+function bubble_chart(value) {
+    d3.json(sample_url).then(function(data) {
+        let results = data.samples.filter(x => x.id == value);
+    let trace_bubble = [{
+        x: results[0].otu_ids,
+        y: results[0].sample_values,
+        mode: "markers",
+        text: results[0].otu_labels,
+        marker:{
+            color: results[0].otu_ids,
+            size: results[0].sample_values,
+            colorscale: 'Earth',
+        },
+    },];
+    
+    let layout = {
+        xaxis: {title: "OTU ID"}
+    };
+
+
+    Plotly.newPlot("bubble", trace_bubble, layout);
+    }
+)};
 
 
 
+//Display the sample metadata, i.e., an individual's demographic information.
+function demographic(values) {
+    d3.json(sample_url).then(function(data) {
+        let metadata = data.metadata;
+
+        // Filter data
+        let result = metadata.filter(x => x.id == values)[0];
+        let demographicInfo = d3.select('#sample-metadata');
+
+        // Clear existing data in demographicInfo
+        demographicInfo.html('');
+
+        // Add key and value pair to the demographicInfo panel
+        Object.entries(result).forEach(([key, value]) => {
+            demographicInfo.append('h6').text(`${key}: ${value}`);
+        });
+    });
+}
+//change charts function
+function optionChanged(value) {
+
+    bar_chart(value);
+    bubble_chart(value);
+    demographic(value);
+  
+}
+//initialize charts
+function Init() {
+
+    // dropdown
+    let selector = d3.select('#selDataset');
+
+    d3.json(sample_url).then(data => {
+        let sampleNames = data.names;
+
+        // add values to drop down
+        for (let i = 0; i < sampleNames.length; i++) {
+            let sampleId = sampleNames[i];
+            selector.append('option').text(sampleId);
+        };
+
+        //first value to charts
+        let initialId = 940;
+
+        // Draw the charts
+        bar_chart(initialId);
+        demographic(initialId);
+        bubble_chart(initialId);
+        
+        
+
+    });
+}
+//initialize function
+Init();
 
 
 
